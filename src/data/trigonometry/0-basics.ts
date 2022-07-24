a447de7030a610951d7095e4f265d996c8a75ab1
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-07-24 20:18:35 +0100

    Fix asciimath

--- src/data/trigonometry/0-basics.ts
+++ src/data/trigonometry/0-basics.ts
@@ -17,21 +17,21 @@ export const trig0: [Question, ...Question[]] = [
     text: "Evaluate `sin(pi/3)`",
     acceptedAnswers: ["sqrt3/2"],
   },
-    //
+  //
   {
     text: "Evaluate `sin(pi/2)`",
     acceptedAnswers: ["1"],
   },
   {
-    text: "Evaluate `sin({2pi}/3)`",
+    text: "Evaluate `sin((2pi)/3)`",
     acceptedAnswers: ["sqrt3/2"],
   },
   {
-    text: "Evaluate `sin({3pi}/4)`",
+    text: "Evaluate `sin((3pi)/4)`",
     acceptedAnswers: ["sqrt2/2"],
   },
   {
-    text: "Evaluate `sin({5pi}/6)`",
+    text: "Evaluate `sin((5pi)/6)`",
     acceptedAnswers: ["1/2"],
   },
   {
@@ -39,31 +39,31 @@ export const trig0: [Question, ...Question[]] = [
     acceptedAnswers: ["0"],
   },
   {
-    text: "Evaluate `sin({7pi}/6)`",
+    text: "Evaluate `sin((7pi)/6)`",
     acceptedAnswers: ["-1/2"],
   },
   {
-    text: "Evaluate `sin({5pi}/4)`",
+    text: "Evaluate `sin((5pi)/4)`",
     acceptedAnswers: ["-sqrt2/2"],
   },
   {
-    text: "Evaluate `sin({4pi}/3)`",
+    text: "Evaluate `sin((4pi)/3)`",
     acceptedAnswers: ["-sqrt3/2"],
   },
   {
-    text: "Evaluate `sin({3pi}/2)`",
+    text: "Evaluate `sin((3pi)/2)`",
     acceptedAnswers: ["-1"],
   },
   {
-    text: "Evaluate `sin {5pi}/3`",
+    text: "Evaluate `sin((5pi)/3)`",
     acceptedAnswers: ["-sqrt3/2"],
   },
   {
-    text: "Evaluate `sin {7pi}/4`",
+    text: "Evaluate `sin((7pi))/4`",
     acceptedAnswers: ["-sqrt2/2"],
   },
   {
-    text: "Evaluate `sin {11pi}/6`",
+    text: "Evaluate `sin((11pi)/6`",
     acceptedAnswers: ["-1/2"],
   },
 ];
