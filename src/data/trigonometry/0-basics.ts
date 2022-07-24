c945d5e7fd0bd17576267b19bd41fb2d0152032e
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-07-24 17:32:19 +0100

    Add more sin problems

--- src/data/trigonometry/0-basics.ts
+++ src/data/trigonometry/0-basics.ts
@@ -2,13 +2,68 @@ import { Question } from "../../model/Question";
 
 export const trig0: [Question, ...Question[]] = [
   {
-    id: "TRIG_01",
-    text: "Evaluate `sin(pi/6)`",
-    acceptedAnswers: ["1/2", "0.5"],
+    text: "Evaluate `sin 0`",
+    acceptedAnswers: ["0"],
   },
   {
-    id: "TRIG_02",
-    text: "Evaluate `cos(pi/6)`",
+    text: "Evaluate `sin(pi/6)`",
+    acceptedAnswers: ["1/2"],
+  },
+  {
+    text: "Evaluate `sin(pi/4)`",
+    acceptedAnswers: ["sqrt2/2"],
+  },
+  {
+    text: "Evaluate `sin(pi/3)`",
     acceptedAnswers: ["sqrt3/2"],
+  },
+    //
+  {
+    text: "Evaluate `sin(pi/2)`",
+    acceptedAnswers: ["1"],
+  },
+  {
+    text: "Evaluate `sin({2pi}/3)`",
+    acceptedAnswers: ["sqrt3/2"],
+  },
+  {
+    text: "Evaluate `sin({3pi}/4)`",
+    acceptedAnswers: ["sqrt2/2"],
+  },
+  {
+    text: "Evaluate `sin({5pi}/6)`",
+    acceptedAnswers: ["1/2"],
+  },
+  {
+    text: "Evaluate `sin pi`",
+    acceptedAnswers: ["0"],
+  },
+  {
+    text: "Evaluate `sin({7pi}/6)`",
+    acceptedAnswers: ["-1/2"],
+  },
+  {
+    text: "Evaluate `sin({5pi}/4)`",
+    acceptedAnswers: ["-sqrt2/2"],
+  },
+  {
+    text: "Evaluate `sin({4pi}/3)`",
+    acceptedAnswers: ["-sqrt3/2"],
+  },
+  {
+    text: "Evaluate `sin({3pi}/2)`",
+    acceptedAnswers: ["-1"],
+  },
+  {
+    text: "Evaluate `sin {5pi}/3`",
+    acceptedAnswers: ["-sqrt3/2"],
+  },
+  {
+    text: "Evaluate `sin {7pi}/4`",
+    acceptedAnswers: ["-sqrt2/2"],
+  },
+  {
+    text: "Evaluate `sin {11pi}/6`",
+    acceptedAnswers: ["-1/2"],
   },
 ];
