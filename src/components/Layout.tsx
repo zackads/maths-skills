08b4e4f3dbc73db7a7de2f0e7347cadb6b266639
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-07-28 11:22:56 +0100

    Add NavBar + Menu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,102 @@
-import React from "react";
-import { Box, Container } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { NavCrumbs } from "./NavCrumbs";
+import MenuIcon from "@mui/icons-material/menu";
+import BlenderIcon from "@mui/icons-material/Blender";
+import TodayIcon from "@mui/icons-material/Today";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+
+export const MenuDrawer = () => {
+  const [open, setOpen] = useState(false);
+
+  const PageList = () => {
+    return (
+      <Box role="presentation" width={250} onClick={() => setOpen(!open)}>
+        <List>
+          <ListItem disablePadding disabled>
+            <ListItemButton>
+              <ListItemIcon>
+                <TodayIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Today's practice"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <BlenderIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Custom session"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding disabled>
+            <ListItemButton>
+              <ListItemIcon>
+                <FitnessCenterIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Mistakes review"} />
+            </ListItemButton>
+          </ListItem>
+          <Divider />
+        </List>
+      </Box>
+    );
+  };
+
+  return (
+    <>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        onClick={() => setOpen(!open)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+        <PageList />
+      </Drawer>
+    </>
+  );
+};
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <NavCrumbs />
-        {children}
-      </Box>
-    </Container>
+    <>
+      <AppBar component="nav" position="static">
+        <Container>
+          <Toolbar>
+            <MenuDrawer />
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Maths Skills
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Container>
+        <Box component="main" sx={{ p: 3 }}>
+          <NavCrumbs />
+          {children}
+        </Box>
+      </Container>
+    </>
   );
 };
